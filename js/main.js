db9720e7c1caa536de18354d465ebac7e9cc03f4
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2023-06-16 12:36:54 +0500

    правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(".header__burgir").click(function() {
-    
     if($(".header__burgir").hasClass("active")) {
         $(".header__navbar").removeClass("open");
         $(".header__burgir").removeClass("rotate");
@@ -13,4 +12,16 @@ $(".header__burgir").click(function() {
             $(".header__burgir").addClass("rotate");
         }, 300);
     }
+});
+$(".modal-open").click(function() {
+    $(".modal").addClass("active");
+    setTimeout(function() {
+        $(".modal").addClass("open");
+    },1);
+});
+$(".modal__close").click(function() {
+    $(".modal").removeClass("open");
+    setTimeout(function() {
+        $(".modal").removeClass("active");
+    },300);
 });
